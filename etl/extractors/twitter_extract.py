2bb93b4a7785130f5ec84ccbb20cc38103b7f3c0
Jhenner Tigreros <jtigreros16@gmail.com>
2020-12-03 10:54:21 -0500

    feat: complete et phases, note: only I just need to load data to database

--- etl/extractors/twitter_extract.py
+++ etl/extractors/twitter_extract.py
@@ -3,29 +3,30 @@ import datetime
 from functools import reduce
 
 def load():
-    final_date = datetime.datetime.today()
-    initial_date = final_date - datetime.timedelta(days = 5)
+    base_date = datetime.datetime.today()
+    date_list = [ base_date - datetime.timedelta(days = x) for x in range(5)]
+    date_list.reverse()
     all_tweets = []
-    for delta in range(5, 0, -1):
-        date_0 = initial_date + datetime.timedelta(days = delta)
-        date_1 = initial_date + datetime.timedelta(days = delta + 1)
-        search_args = load_credentials(filename="./configs/twitter_api.yaml",
-                        yaml_key="search_tweets_v2",
-                        env_overwrite=False)
+    for idx, date in enumerate(date_list):
+        if idx != 4:
+            final_date = date + datetime.timedelta(days = 1)
+            search_args = load_credentials(filename="./configs/twitter_api.yaml",
+                            yaml_key="search_tweets_v2",
+                            env_overwrite=False)
 
-        query = gen_request_parameters("covid19 colombia lang:es", results_per_call=100,
-                    place_fields='country', start_time=date_0.strftime('%Y-%m-%d'), end_time=date_1.strftime('%Y-%m-%d'))
+            query = gen_request_parameters("covid19 colombia lang:es", results_per_call=100,
+                        place_fields='country', start_time=date.strftime('%Y-%m-%d'), end_time=final_date.strftime('%Y-%m-%d'))
 
-        tweets = collect_results(query,
-                                max_tweets=1000,
-                                result_stream_args=search_args)
+            tweets = collect_results(query,
+                                    max_tweets=1000,
+                                    result_stream_args=search_args)
 
-        def add_date(x):
-            x['fecha'] = date_0.strftime('%Y-%m-%d')
+            def add_date(x):
+                x['fecha'] = date.strftime('%Y-%m-%d')
 
-            return x
-        tweets = list(map(add_date, tweets))
-        all_tweets.append(tweets)
+                return x
+            tweets = list(map(add_date, tweets))
+            all_tweets.append(tweets)
 
     all_tweets = reduce(lambda x, y: x + y, all_tweets)
     return all_tweets
